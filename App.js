25f6a246372cbe9bf1f2cc7ba4727e3ba4860deb
Agustin Mariscotti <agustinm06@gmail.com>
2021-02-03 11:24:08 -0300

    Add reactnavigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const[color, setColor]=useState('blue')
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
